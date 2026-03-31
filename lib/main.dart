bb48eccd8a7acdedb8fb53d4acd52ae0ea46b9ba
iyed_mak <iyed_mak@kali.Kali>
2026-03-31 07:16:52 -0400

    main initial project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const QuranApp());
+}
+
+class QuranApp extends StatelessWidget {
+  const QuranApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quran App',
+      home: const LoginPage(),
+    );
+  }
+}
